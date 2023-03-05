1be8bfa355eafb8103a70c8b1a3c4492330dd49c
Toba-dean <tobaogundimu@gmail.com>
2023-03-05 20:50:54 +0100

    added react router dom for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>
+  },
+  {
+    path: "/register",
+    element: <div>Register Page</div>
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
 }
 
-export default App;
+export default App

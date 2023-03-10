87361546c4b5519267427fbfdd2723bf00614b1f
Toba-dean <tobaogundimu@gmail.com>
2023-03-10 21:31:19 +0100

    added the registeration UI and all the necessary validation logic

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -26,6 +26,16 @@ export async function resetPasswordValidation(values) {
   return errors;
 }
 
+/** validate register form */
+export async function registerValidation(values) {
+  const errors = verifyUsername({}, values);
+  passwordVerify(errors, values);
+  emailVerify(errors, values);
+
+  return errors;
+}
+
+
 
 /** ************************************************* */
 
@@ -53,6 +63,19 @@ function verifyUsername(error = {}, values) {
     error.username = toast.error("Username Required");
   } else if (values.username.includes(" ")) {
     error.username = toast.error("Invalid Username.")
+  }
+
+  return error;
+}
+
+/** validate email */
+function emailVerify(error = {}, values) {
+  if (!values.email) {
+    error.email = toast.error("Email Required.");
+  } else if (values.email.includes(" ")) {
+    error.email = toast.error("Email cannot contain space.")
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    error.email = toast.error("Invalid email address.")
   }
 
   return error;

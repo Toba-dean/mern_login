1e73797372fb04c16564b9275f7f01125a70bc60
Toba-dean <tobaogundimu@gmail.com>
2023-03-12 11:57:40 +0100

    started the backend logic, added the connection to mongo db

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
-console.log("Hello America!")
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const morgan = require("morgan");
+const PORT = 3000;
+
+const connectDB = require("./db/connect");
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"))
+app.disable("x-powered-by")
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, msg: "This is the get route" })
+})
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server connected to: http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+start();
